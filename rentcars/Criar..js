4fb1a4b7b2234ab3d31cc0e3ecc40a5804eac268
Winicius Neves <127152668+WiniciusNeves@users.noreply.github.com>
2023-10-31 15:07:04 -0300

    Delete rentcars directory

--- rentcars/Criar..js
+++
@@ -1,9 +0,0 @@
-function PaginaInicial(){
-    var form = document.getElementById("formCadastro");
-    var dados = new FormData(form);
-
-    fetch("insere-produto.php", {
-        method: "POST",
-        body: dados
-    });
-}
